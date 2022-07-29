f2ccaefbd79733eb17465b42384bd0c2933b3ef0
Tobias Leonel Casazza <tobiascasazza@gmail.com>
2022-07-28 22:15:40 -0300

    Boton Reiniciar

--- src/componentes/GalanNeoGalan/GalanNeoGalan.js
+++ src/componentes/GalanNeoGalan/GalanNeoGalan.js
@@ -3,6 +3,9 @@ import "../../styles/galanNeo.css"
 import Nivel from "./Nivel";
 import { preguntas } from "../../datos/preguntasGalanNeo";
 import Resolucion from "./Resolucion";
+import Carta from "./Carta";
+import { Button } from "@mui/material";
+import { textAlign } from "@mui/system";
 
 export default function GalanNeoGalan() {
     const [contadorPreguntas, setContadorPreguntas] = React.useState(0);
@@ -11,8 +14,20 @@ export default function GalanNeoGalan() {
     const [puntosMedioGalan, setPuntosMedioGalan] = React.useState(0);
     const [puntosMedioNeo, setPuntosMedioNeo] = React.useState(0);
 
+    function reiniciar(){
+        setContadorPreguntas(0);
+        setPuntosGalan(0);
+        setPuntosNeo(0);
+        setPuntosMedioGalan(0);
+        setPuntosMedioNeo(0);
+    }
+
     return (
         <>
+            <div style={{textAlign: 'initial'}}>
+            <Button variant="outlined" color='error' onClick={reiniciar}>Reiniciar</Button>
+            </div>
+            
             {
                 contadorPreguntas >= preguntas.length ?
                     <Resolucion 
